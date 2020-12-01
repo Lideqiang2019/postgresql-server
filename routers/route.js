38f5cb7252b6c15ec95a90b697ba94ef8b33c765
Lideqiang2019 <17805422918@163.com>
2020-12-01 23:00:34 +0800

    返回geojson数据

--- routers/route.js
+++ routers/route.js
@@ -121,43 +121,36 @@ router.post('/age', (req, res) => {
 router.post('/getpolygon', (req, res) => {
   // const { name } = req.query
   const {polygon} = req.body
-  const polygon3 = 'POLYGON((114.07614815496311 22.558253845914187, \
-    114.07449924442668 22.551049590720183,\
-    114.0911532408435 22.550034876665336,\
-    114.09054864031386 22.558608975523665,\
-    114.07614815496311 22.558253845914187))'
   console.log("polygon",polygon)
   array = 'POLYGON(('
-  turf.coordEach(polygon4, function (currentCoord, coordIndex, featureIndex, multiFeatureIndex, geometryIndex) {
-    //=currentCoord
-    //=coordIndex
-    //=featureIndex
-    //=multiFeatureIndex
-    //=geometryIndex
-    console.log("11",currentCoord)
+  turf.coordEach(polygon, function (currentCoord, coordIndex, featureIndex, multiFeatureIndex, geometryIndex) {
     array += currentCoord[0] + ' ' + currentCoord[1] + ','
-    // console.log("22",currentCoord[0])
   });
   array = array.substring(0,array.lastIndexOf(','))
   array+='))'
-  // const polygon2 = 'POLYGON($points)`
-  // console.log(polygon2)
-  // console.log(height1,height2)
   pool.connect(function (err, client, done) {
     if (err) {
+        res.send({code:1,msg:'数据库未响应'})
         return console.error(err);
+
     }
     // 简单输出个 Hello World select ST_AsText(geom) from demojz where height=$1 or height=$2
-    client.query('SELECT ST_AsGeoJson(geom)::jsonb,block_use FROM land1029 WHERE ST_Intersects(\
+    client.query('SELECT ST_AsGeoJson(geom)::jsonb As geometry,block_use,block_name FROM land1029 WHERE ST_Intersects(\
       ST_GeomFromText($1, 0), geom)', [array], function (err, result) {
         done();// 释放连接（将其返回给连接池）
         if (err) {
+          res.send({code:1,msg:'查询错误'})
             return console.error('查询出错', err);
         }
-        const geodata = result.rows[0]['st_asgeojson']['coordinates']
-        const geojsondata = turf.polygon(geodata,{block_use:result.rows[0]['block_use']})
-        console.log("geojsondata",geojsondata)
-        res.send(result.rows)
+
+        geojsonArr = result.rows.reduce((pre,val)=>{
+          pre.push(turf.polygon(val['geometry']['coordinates'],{block_use:val['block_use'],block_name:val['block_name']}))
+          return pre
+        },[])
+        
+        const geojsaonAll = turf.featureCollection(geojsonArr)
+        console.log("geojsondata",geojsaonAll)
+        res.send({code:0,data:geojsaonAll})
     });
   });
 })
@@ -195,6 +188,15 @@ function TransToPolygon(){
   array = array.substring(0,array.lastIndexOf(','))
   array+='))'
   console.log(array)
+  result = [{'a':1,'b':2},{'a':3,'b':4}]
+  const data = result.reduce((pre,index)=>{
+    pre.push(index['a'])
+    console.log("index",index)
+    console.log(pre)
+    return pre
+  },[])
+
+  console.log(data)
 }
 // TransToPolygon()
 
